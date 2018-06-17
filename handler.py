181b97ae50007b4f05a03ebd46f2a407decefebf
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-06-16 21:19:52 -0600

    process json representation

--- handler.py
+++ handler.py
@@ -1,15 +1,25 @@
 import sys
 import time
+import json
 
 def get_stdin():
-    buf = ""
+    # extract the lines
+    req = ""
     for line in sys.stdin:
-        buf = buf + line
-    return buf
+        req = req + line
+
+    #convert the json representation into a python object
+    json_req = json.loads(req)
+    
+    # extract key and value
+    result = {"key": json_req["key"], "value": json_req["value"]}
+    
+    # serialize to a JSON formatted string 
+    str_rep = json.dumps(result)
+    print str_rep
 
 if(__name__ == "__main__"):
     st = get_stdin()
-    print(st)
     print 'Sleeping for 50 miliseonds!.'
     time.sleep(0.05)
     print 'Sleeping completed.'
